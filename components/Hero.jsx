b6dd910a996b37e2c9849b62b643541174f4eb2a
GSSS-Mirzewala <gsssmirzewala1954@gmail.com>
2026-02-24 21:56:46 +0530

    Delete components directory

--- components/Hero.jsx
+++
@@ -1,9 +0,0 @@
-function Hero({ children }) {
-  return (
-    <div className="flex flex-col w-[94%] md:w-[96%] m-[3%] md:m-[2%] gap-4">
-      {children}
-    </div>
-  );
-}
-
-export default Hero;
